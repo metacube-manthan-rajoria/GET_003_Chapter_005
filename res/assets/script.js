c8a9f53c09ff33f4c8be18f9a047fef27ebb10e4
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-12-16 12:34:21 +0530

    made app responsive

--- res/assets/script.js
+++ res/assets/script.js
@@ -1,7 +1,25 @@
 let navBar = document.getElementById("side_nav");
 let navBarButton = document.getElementById("nav_hamburger");
+let appWrapper = document.getElementById("app_wrapper");
+let sideCloseX = document.getElementById("side_nav_x");
 let navIsOpen = true;
 navBarButton.addEventListener("click", ()=>{
+    navIsOpen = !navIsOpen;
+    if(navIsOpen){
+        if(window.innerWidth > 780){
+            appWrapper.style.gridTemplateColumns = "2fr 10fr";
+            navBar.style.display = "none";
+        }
+        navBar.style.display = "block";
+    }else{
+        if(window.innerWidth > 780){
+            appWrapper.style.gridTemplateColumns = "1fr";
+            navBar.style.display = "none";
+        }
+    }
+});
+
+sideCloseX.addEventListener("click", ()=>{
     navIsOpen = !navIsOpen;
     if(navIsOpen){
         navBar.style.display = "block";
@@ -9,4 +27,17 @@ navBarButton.addEventListener("click", ()=>{
         navBar.style.display = "none";
     }
 });
+
+window.addEventListener("resize", ()=>{
+    if(window.innerWidth <= 780){
+        appWrapper.style.gridTemplateColumns = "1fr";
+    }else{
+        if(navIsOpen){
+            appWrapper.style.gridTemplateColumns = "2fr 10fr";
+            
+        }else{
+            appWrapper.style.gridTemplateColumns = "1fr";
+        }
+    }
+})
 
